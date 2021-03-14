7fe76b6f277e658fb6507de4cb428d56a1e01ac5
mr-d0nx <76074196+mr-d0nx@users.noreply.github.com>
2021-03-14 09:34:52 +0700

    Create open.sh

---
+++ open.sh
@@ -0,0 +1,133 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $cyan
+read -p "Masuk tools? (y/n) : " tools
+echo $cyan"Menginstall Paket..."
+pkg install figlet -y
+clear
+echo $cyan
+figlet "VIRUS"
+sleep 1
+echo $cyan"Kirim Virus"
+sleep 1                                        echo
+echo $cyan"+------------------------------+"   echo $cyan" Author : MrD0nx"
+echo $cyan" IG     : Moh.dito_"
+echo $cyan" Team   : IndoDarknet"
+echo $cyan"+------------------------------+"
+echo
+sleep 1
+echo $cyan"Selamat Datang Di Tools"
+sleep 1
+echo $cyan
+echo $cyan"Nama Kamu"
+sleep 1
+read -p "-----------> " nama
+echo $cyan"Oke, Halo $nama ;)"
+sleep 1
+clear
+figlet "Virus"
+echo "By MrD0nx"
+echo "===================="
+echo "Menu :"
+echo "1). Via IP"
+echo "2). Via No Telepon"
+echo "3). Keluar"
+echo "===================="
+echo
+read -p "Pilihan -----> " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+sleep 1
+figlet "VIA IP"
+read -p "IP Target ---> " ip
+echo "Tunggu Sebentar..."
+sleep 1
+echo
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $yellow"Mengirim ke ip $ip"
+sleep 1
+echo $cyan
+sleep 1
+echo "Sukses Mengirim 15 Virus ke ip $ip"
+sleep 1
+clear
+sleep 1
+echo "Sukses Terus $nama"
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+sleep 1
+echo $cyan                                     figlet "VIA NO"
+read -p "Masukan No Target ---> " hp
+echo
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $yellow"Mengirim Virus ke nomor $hp"
+sleep 1
+echo $cyan
+echo "sukses mengirim virus ke no $hp"
+sleep 1
+clear
+sleep 1
+echo "Sukses terus $nama"
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $cyan"Sukses Terus $nama"
+exit
+fi
